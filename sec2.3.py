dddf75a0559728d1fa1afe978164bb8f8212cdbf
Roberto Andrade <randradep@usfq.edu.ec>
2026-06-29 10:55:16 -0500

    Add happy_new_year function for New Year wishes

--- sec2.3.py
+++ sec2.3.py
@@ -1,1 +1,12 @@
+def happy_new_year(wishes = True):
+    print("Tres...")
+    print("Dos...")
+    print("Uno...")
+    if not wishes:
+        return
+
+    print("¡Feliz año nuevo!")
+
+happy_new_year()
+
 
